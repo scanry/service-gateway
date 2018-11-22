40ef728187dec88cd202909b3388f499cc5ed0ed
sixliu <359852326@qq.com>
2018-11-22 11:37:48 +0800

    更新

--- src/main/java/com/sixliu/gateway/StartUp.java
+++ src/main/java/com/sixliu/gateway/StartUp.java
@@ -2,6 +2,7 @@ package com.sixliu.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+
 /**
  * @author:MG01867
  * @date:2018年6月15日
@@ -10,7 +11,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
  * @describe 程序启动入口类
  */
 @SpringCloudApplication
-public class StartUp{
+public class StartUp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartUp.class);
